5de42195f6fb0bbb693f9ae5b358914ca75822f0
Jaehun Kim <dev4jaehun.kim@gmail.com>
2022-01-17 21:23:46 +0900

    백준 문제풀이 D+7, 해싱(15829)

---
+++ d7-hashing.py
@@ -0,0 +1,18 @@
+# Algorithm Study D+7
+# Difficulty: Class 2, Bronze 2
+# Problem URL: https:/www.acmicpc.net/problem/15829
+
+alphaDict = {
+    "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7,
+    "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14,
+    "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21,
+    "v": 22, "w": 23, "x": 24, "y": 25, "z": 26
+}
+L = int(input())
+sum = 0
+str = input()
+
+for i in range(L):
+    sum += alphaDict[str[i]] * 31**i
+
+print(sum % 1234567891)
